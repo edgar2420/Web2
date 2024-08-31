252425a152286150c82951479dca2e06d3e3d2d5
edgar2420 <edgarrojas082@gmail.com>
2024-08-31 16:43:40 -0400

    practica de cursos con node

---
+++ ApiPractica/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "proyectoweb1",
+    dialect: "mysql",
+}
---
+++ ApiPractica/models/usuario.model.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, Sequelize) => {
+    const Usuario = sequelize.define("usuario", {
+        nombre: {
+            type: Sequelize.STRING,
+        },
+        apellido: {
+            type: Sequelize.STRING,
+        },
+        email: {
+            type: Sequelize.STRING,
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+    })
+    return Usuario;
+};
